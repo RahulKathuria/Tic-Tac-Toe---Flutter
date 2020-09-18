d976f268ba14eab09cb53e3f53249dc244afbae7
RahulKathuria <rahulkathuria52@gmail.com>
2020-09-18 23:29:17 +0530

    Added Grid view

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: GridView.builder(
+          itemCount: 9,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Center(
+                child: Text(index.toString()),
+              ),
+            );
+          }),
+    );
+  }
+}
